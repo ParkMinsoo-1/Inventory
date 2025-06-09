23afbc35fd824e12f5fe34773ced8fd3cd1ca541
ParkMinsoo <harimaho88@gmail.com>
2025-06-09 21:15:43 +0900

    인벤토리 테스트

--- Assets/Scripts/UI/UIInventory.cs
+++ Assets/Scripts/UI/UIInventory.cs
@@ -11,7 +11,7 @@ public class UIInventory : MonoBehaviour
 
     void Start()
     {
-        SetInventoryUI(16);
+        SetInventoryUI(10);
         UpdateInventoryUI(GameManager.Instance.player.inventory);
     }
 
