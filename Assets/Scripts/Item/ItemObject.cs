522955fdfe4fdfd479bdf2616eae922c2e40ef6d
ParkMinsoo <harimaho88@gmail.com>
2025-06-09 17:03:41 +0900

    아이템 데이터 추가

--- Assets/Scripts/Item/ItemObject.cs
+++ Assets/Scripts/Item/ItemObject.cs
@@ -6,6 +6,15 @@ public class ItemObject : MonoBehaviour
 {
     public ItemData itemData;
     
-    
+    private void Start()
+    {
+        itemData = Resources.Load<ItemData>("ItemData/" + gameObject.name);
+
+        if (itemData == null)
+        {
+            Debug.Log($"{gameObject.name}을 찾을 수 없습니다.");
+        }
+        
+    }
     
 }
